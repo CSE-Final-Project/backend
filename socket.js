ab28ca342c13d9b483202930ab28c743d2d3bc9d
MinjaeJo1999 <rycbar1999@naver.com>
2022-02-14 10:12:19 +0900

    [2월 14일] 비디오 중복 문제

--- socket.js
+++ socket.js
@@ -4,9 +4,8 @@ module.exports = ((io) =>
     const users = {};  
     const socketToRoom = {}; 
 
-    io.on('connection', socket => { 
+    io.on('connection', socket => {  
         socket.on("join room", roomID => { 
-            socket.join(roomID)
             if (users[roomID]) { 
                 const length = users[roomID].length;
                 if (length === 4) {
@@ -17,14 +16,12 @@ module.exports = ((io) =>
             } else { 
                 users[roomID] = [socket.id];
             }
-            
-            const myPeerID = socket.id; 
+            const myPeerID = socket.id;  
             io.to(socket.id).emit("my peer id", myPeerID);
-    
             socketToRoom[socket.id] = roomID;
-            const usersInThisRoom = users[roomID]//.filter(id => id !== socket.id); 
-            //socket.emit("all users", usersInThisRoom); 
-            io.to(roomID).emit("all users", usersInThisRoom)
+            const usersInThisRoom = users[roomID].filter(id => id !== socket.id); 
+            socket.emit("all users", usersInThisRoom); 
+    
         });
     
         socket.on("sending signal", payload => {
@@ -37,18 +34,24 @@ module.exports = ((io) =>
     
         socket.on('false-event', data => {
             var roomID = data.dst_room;
-            socket.to(roomID).emit('video-state', {peer_tf: data.peer_tf, tf_state: data.tf_state})
+            const usersInThisRoom = users[roomID].filter(id => id !== socket.id); 
+            for( let i=0; i < usersInThisRoom.length ; i++){
+                socket.to(usersInThisRoom[i]).emit('video-state', {peer_tf: data.peer_tf, tf_state: data.tf_state});
+            }
         })
-     
+            
         socket.on('disconnect', () => {
-            var roomID = socketToRoom[socket.id]; 
-            socket.to(roomID).emit('user-disconnected', socket.id)
+            var roomID = socketToRoom[socket.id];
+            var usersInThisRoom = users[roomID].filter(id => id !== socket.id); 
+            for( let i=0; i < usersInThisRoom.length ; i++){
+                socket.to(usersInThisRoom[i]).emit('user-disconnected', socket.id );
+            }
+           
             let room = users[roomID];
             if (room) {
                 room = room.filter(id => id !== socket.id);
                 users[roomID] = room;
             }
-    
         });
     
     });
