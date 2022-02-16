a50de43c68a694a09eb6ba93b63f4af83d608f0f
woozoo_zinc <your_zinc@ewhain.net>
2022-02-16 16:43:41 +0900

    [2월 16일] Attendance Check + Penalty Update

--- app.js
+++ app.js
@@ -64,26 +64,45 @@ app.use(cors({
 const schedule = require('node-schedule');
 const { QueryTypes } = require('sequelize');
 
-// Attendance Check
-const attendance_check_time = '59 23 * * *'
+// Studytime Initialize
+const studytime_init_time = '43 15 * * *'
+const studytime_init = schedule.scheduleJob(studytime_init_time, async () => {
+    let query =
+    `SELECT user_id, study_id FROM user_studies`
+
+    const user_studies = await models.sequelize.query(query,{type: QueryTypes.SELECT})
+
+    const t = new Date()
+    t.setUTCHours(0, 0, 0, 0)
+
+    for (i of user_studies){
+        await models.studytime.create({
+            user_id: i.user_id,
+            study_id: i.study_id,
+            studytime: 0,
+            date: t
+        })
+    }
+})
+
+// Attendance Check + Penalty Update
+const attendance_check_time = '41 41 16 * * *'
 const attendance_check = schedule.scheduleJob(attendance_check_time, async () => {
     let query =
-    `SELECT user_id, study_id, target_time, date, studytime FROM studytimes
+    `SELECT user_id, study_id, target_time, studytime FROM studytimes
             JOIN studies ON studytimes.study_id = studies.id
             WHERE Date(studytimes.date) = CURDATE()`
         
-    const studytime = await models.sequelize.query(
-        query,
-        {   type: QueryTypes.SELECT   }
-    )
+    const studytime = await models.sequelize.query(query,{type: QueryTypes.SELECT})
+    const t = new Date()
 
     for (i of studytime){
-        if ( i.target_time > i.studytime ){
+        if ( i.target_time <= i.studytime ){
             // Attendance
             await models.attendance.create({
                 user_id: i.user_id,
                 study_id: i.study_id,
-                date: i.date,
+                date: t,
                 attendance: 0
             })
         }
@@ -92,9 +111,23 @@ const attendance_check = schedule.scheduleJob(attendance_check_time, async () =>
             await models.attendance.create({
                 user_id: i.user_id,
                 study_id: i.study_id,
-                date: i.date,
+                date: t,
                 attendance: 1
             })
+            // Penalty
+            const study_penalty = await models.study.findOne({
+                attribute: ['penalty'],
+                where: { id: i.study_id }
+            })
+            const before = await models.penalty.findOne({
+                attribute: ['total_penalty'],
+                where: { user_id: i.user_id, study_id: i.study_id }
+            })
+            // update
+            await models.penalty.update(
+                { total_penalty: study_penalty.penalty + before.total_penalty },
+                { where: { user_id: i.user_id, study_id: i.study_id } }
+            )
         }
     }
 })
